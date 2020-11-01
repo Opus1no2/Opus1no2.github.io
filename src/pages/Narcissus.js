4b822f4ef9d2e909495162131b8772834fe6dc87
Opus1no2 <tillotson.travis@gmail.com>
2020-11-01 09:56:53 -0600

    narcissus dynamic background

--- src/pages/Narcissus.js
+++ src/pages/Narcissus.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
-const Narcissus = () => {
-  const Cont = styled.div`
+const Cont = styled.div`
     min-height: 100vh;
     display: flex;
     flex-direction: column;
@@ -10,7 +9,7 @@ const Narcissus = () => {
     align-items: center;
   `;
 
-  const Video = styled.video`
+const Video = styled.video`
     transform: scaleX(-1);
     border-radius: 80px;
     border: solid 10px #609e1c;
@@ -22,6 +21,18 @@ const Narcissus = () => {
       0 0 0 30px hsl(150, 100%, 90%);
   `;
 
+const Canvas = styled.canvas`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`
+const Narcissus = () => {
+  const [color, setColor] = useState(0);
+  const [dir, setDir] = useState('');
+
+  const canvasRef = useRef();
   const videoRef = useRef();
 
   useEffect(() => {
@@ -33,8 +44,41 @@ const Narcissus = () => {
     });
   }, [videoRef]);
 
+  useEffect(() => {
+    const changeColor = () => {
+      if (color === 0) {
+        setDir('inc');
+      }
+      if (color === 255) {
+        setDir('dec');
+      }
+
+      if (dir === 'inc') {
+        setColor(color => color + 1);
+      }
+      if (dir === 'dec') {
+        setColor(color => color - 1);
+      }
+    };
+
+    let _id = setInterval(changeColor, 100);
+
+    return () => clearInterval(_id);
+  }, [color, dir]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ctx.fillStyle = `hsl(${color}, 100%, 25%)`;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }, [color]);
+
   return (
     <Cont>
+      <Canvas ref={canvasRef} />
       <Video ref={videoRef}></Video>
     </Cont>
   );
